9350a5b35dd3ee0aa202b0b9b2477b73d13e9dea
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-02-03 00:17:56 +0900

    fix page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,5 +5,5 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <h1 className="styles.ttl">CUBE</h1>
+  return <h1 className={styles.ttl}>CUBE</h1>
 }
