b2eddab76bef7c841df4fd431f77ecabd7abbc32
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-02-03 00:04:47 +0900

    add function about

--- src/app/about.js
+++ src/app/about.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function about() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
