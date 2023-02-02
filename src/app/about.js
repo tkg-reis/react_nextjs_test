e7c2903ddf0206250728c673d48b497124091a62
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-02-02 23:52:38 +0900

    add pra

--- src/app/about.js
+++ src/app/about.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          ようこそnext.jsへ&nbsp;
+          わたしについて&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
